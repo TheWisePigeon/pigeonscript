cfbf6a11e346fe08470b7425133371a503e8331c
TheWisePigeon <zozozozeph@gmail.com>
2022-04-18 18:01:17 +0000

    set up tailwind

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+
+<script>
+    import "../../app.css"
+</script>
+
+<template>
+    <div class=" flex">
+        <p class=" font-extrabold text-4xl dark:text-violet-700">
+            Welcome to my blog!!
+        </p>
+
+    </div>
+</template>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+</script>
+
+<Header/>
+
+<div class=" bg-red-500">
+    <slot></slot>
+</div>
+
